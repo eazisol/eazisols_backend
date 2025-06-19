2753d7a93a630639272900926e6c2702eae73285
ahsan-eazisols <hannan.shahid@eazisols.com>
2025-06-19 12:12:01 +0500

    eazisols backend project setup

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>@yield('title')</title>
+
+  <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+  
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="{{ asset('assets/css/app.min.css') }}">
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/components.css') }}">
+  <!-- Custom style CSS -->
+  <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
+  <link rel='shortcut icon' type='image/x-icon' href="{{ asset('assets/img/favicon.ico') }}" />
+</head>
+<body>
+    <div class="loader"></div>
+    <div id="app">
+            <div class="main-wrapper main-wrapper-1">
+                @if (!request()->is('login'))
+                @include('partials.header')
+                @endif
+
+                @if (!request()->is('login'))
+                  @include('partials.sidebar')
+                @endif
+
+                
+                <div class="main-content">
+                    <section class="section">
+                        @yield('content')
+                    </section>
+                </div>
+
+                @if (!request()->is('login'))
+                @include('partials.footer')
+                @endif
+                
+            </div>
+    </div>
+    
+    {{-- Header and Navbar --}}
+
+    
+
+    {{-- js --}}
+    <script src="{{ asset('assets/js/app.min.js') }}"></script>
+  <!-- JS Libraies -->
+  <script src="{{ asset('assets/bundles/apexcharts/apexcharts.min.js') }}"></script>
+  <!-- Page Specific JS File -->
+  <script src="{{ asset('assets/js/page/index.js') }}"></script>
+  <!-- Template JS File -->
+  <script src="{{ asset('assets/js/scripts.js') }}"></script>
+  <!-- Custom JS File -->
+  <script src="{{ asset('assets/js/custom.js') }}"></script>
+</body>
+</html>
