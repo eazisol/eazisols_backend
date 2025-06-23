421f3bd95d5d30e524f4818690086a0b35db81d5
ahsan-eazisols <hannan.shahid@eazisols.com>
2025-06-23 16:04:20 +0500

    header UI issue in blogs and career

--- resources/views/careers/index.blade.php
+++ resources/views/careers/index.blade.php
@@ -206,7 +206,7 @@
 @section('scripts')
 
 <!-- jQuery -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+{{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
 
 <!-- SweetAlert2 -->
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
