055294bb2a8ad70e7a16ed5e0218d5fc67dd3843
ahsan-eazisols <hannan.shahid@eazisols.com>
2025-06-25 12:40:46 +0500

    location filter UI issues

--- resources/views/locations/index.blade.php
+++ resources/views/locations/index.blade.php
@@ -23,72 +23,62 @@
                             </a>
                         </div>
                     </div>
+                    
                     <div class="card-body">
-                        <div class="mt-3 mb-4">
-                            <form action="{{ route('locations.index') }}" method="GET" class="row">
-                                <div class="form-group col-md-3">
-                                    <label for="search" class="sr-only">Search</label>
-                                    <div class="input-group">
-                                        <input type="text" class="form-control" id="search" name="search" 
-                                            placeholder="Search locations..." value="{{ request('search') }}">
+                        <div class="row mb-3">
+                            <div class="col-md-12">
+                                <form action="{{ route('locations.index') }}" method="GET" class="form-inline">
+                                    <div class="input-group mb-2 mr-sm-2">
+                                        <input type="text" name="search" class="form-control" placeholder="Search locations..." value="{{ request('search') }}">
                                         <div class="input-group-append">
-                                            <button class="btn btn-primary" type="submit">
-                                                <i class="fas fa-search"></i>
-                                            </button>
+                                            <button class="btn btn-primary" type="submit"><i class="fas fa-search"></i></button>
                                         </div>
                                     </div>
-                                </div>
-
-                                <div class="form-group col-md-2">
-                                    <label for="city">City</label>
-                                    <select class="form-control" id="city" name="city" onchange="this.form.submit()">
-                                        <option value="">All Cities</option>
-                                        @foreach($cities as $city)
-                                            <option value="{{ $city }}" {{ request('city') == $city ? 'selected' : '' }}>
-                                                {{ $city }}
-                                            </option>
-                                        @endforeach
-                                    </select>
-                                </div>
-
-                                <div class="form-group col-md-2">
-                                    <label for="state">State</label>
-                                    <select class="form-control" id="state" name="state" onchange="this.form.submit()">
-                                        <option value="">All States</option>
-                                        @foreach($states as $state)
-                                            <option value="{{ $state }}" {{ request('state') == $state ? 'selected' : '' }}>
-                                                {{ $state }}
-                                            </option>
-                                        @endforeach
-                                    </select>
-                                </div>
-
-                                <div class="form-group col-md-2">
-                                    <label for="country">Country</label>
-                                    <select class="form-control" id="country" name="country" onchange="this.form.submit()">
-                                        <option value="">All Countries</option>
-                                        @foreach($countries as $country)
-                                            <option value="{{ $country }}" {{ request('country') == $country ? 'selected' : '' }}>
-                                                {{ $country }}
-                                            </option>
-                                        @endforeach
-                                    </select>
-                                </div>
-
-                                <div class="form-group col-md-3">
-                                    <label class="d-block">&nbsp;</label>
-                                    <div class="btn-group">
-                                        <button type="submit" class="btn btn-primary">
-                                            <i class="fas fa-filter mr-1"></i> Filter
-                                        </button>
-                                        @if(request()->anyFilled(['search', 'city', 'state', 'country', 'sort', 'direction']))
-                                            <a href="{{ route('locations.index') }}" class="btn btn-light">
-                                                <i class="fas fa-times mr-1"></i> Clear
-                                            </a>
-                                        @endif
+                                    
+                                    <div class="form-group mb-2 mr-2">
+                                        <select name="city" class="form-control" style="min-width: 150px;">
+                                            <option value="">All Cities</option>
+                                            @foreach($cities as $city)
+                                                <option value="{{ $city }}" {{ request('city') == $city ? 'selected' : '' }}>
+                                                    {{ $city }}
+                                                </option>
+                                            @endforeach
+                                        </select>
                                     </div>
-
-                                    <div class="ml-auto mt-2">
+                                    
+                                    <div class="form-group mb-2 mr-2">
+                                        <select name="state" class="form-control" style="min-width: 150px;">
+                                            <option value="">All States</option>
+                                            @foreach($states as $state)
+                                                <option value="{{ $state }}" {{ request('state') == $state ? 'selected' : '' }}>
+                                                    {{ $state }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    
+                                    <div class="form-group mb-2 mr-2">
+                                        <select name="country" class="form-control" style="min-width: 150px;">
+                                            <option value="">All Countries</option>
+                                            @foreach($countries as $country)
+                                                <option value="{{ $country }}" {{ request('country') == $country ? 'selected' : '' }}>
+                                                    {{ $country }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    
+                                    <button type="submit" class="btn btn-primary mb-2 mr-2">
+                                        <i class="fas fa-filter mr-1"></i> Apply Filters
+                                    </button>
+                                    
+                                    @if(request()->anyFilled(['search', 'city', 'state', 'country']))
+                                        <a href="{{ route('locations.index') }}" class="btn btn-light mb-2">
+                                            <i class="fas fa-times mr-1"></i> Clear Filters
+                                        </a>
+                                    @endif
+                                    
+                                    <div class="ml-auto mb-2">
                                         <div class="btn-group">
                                             <a href="{{ route('locations.index', ['sort' => 'created_at', 'direction' => 'desc'] + request()->except(['sort', 'direction'])) }}" 
                                                class="btn {{ request('sort', 'created_at') == 'created_at' && request('direction', 'desc') == 'desc' ? 'btn-primary' : 'btn-outline-primary' }}">
@@ -100,9 +90,51 @@
                                             </a>
                                         </div>
                                     </div>
-                                </div>
-                            </form>
+                                </form>
+                            </div>
                         </div>
+                        
+                        <div class="px-2">
+                            @if(request()->anyFilled(['search', 'city', 'state', 'country']))
+                                <div class="active-filters mb-3">
+                                    <span class="font-weight-bold mr-2">Active Filters:</span>
+                                    @if(request('search'))
+                                        <span class="badge badge-info mr-1">
+                                            Search: "{{ request('search') }}"
+                                        </span>
+                                    @endif
+                                    
+                                    @if(request('city'))
+                                        <span class="badge badge-info mr-1">
+                                            City: {{ request('city') }}
+                                        </span>
+                                    @endif
+                                    
+                                    @if(request('state'))
+                                        <span class="badge badge-info mr-1">
+                                            State: {{ request('state') }}
+                                        </span>
+                                    @endif
+                                    
+                                    @if(request('country'))
+                                        <span class="badge badge-info mr-1">
+                                            Country: {{ request('country') }}
+                                        </span>
+                                    @endif
+                                </div>
+                            @endif
+                        </div>
+                        
+                        @if(session('success'))
+                            <div class="alert alert-success alert-dismissible show fade">
+                                <div class="alert-body">
+                                    <button class="close" data-dismiss="alert">
+                                        <span>×</span>
+                                    </button>
+                                    {{ session('success') }}
+                                </div>
+                            </div>
+                        @endif
                         
                         <div class="table-responsive">
                             <table class="table table-striped">
@@ -171,28 +203,19 @@
             let form = $(this).closest('form');
             
             Swal.fire({
-                title: 'Are you sure?',
-                text: "You won't be able to revert this!",
+                title: 'Delete Location',
+                text: "Are you sure you want to delete?",
                 icon: 'warning',
                 showCancelButton: true,
                 confirmButtonColor: '#3085d6',
                 cancelButtonColor: '#d33',
-                confirmButtonText: 'Yes, delete it!'
+                confirmButtonText: 'Delete',
             }).then((result) => {
                 if (result.isConfirmed) {
                     form.submit();
                 }
             });
         });
-        
-        // Show success message using SweetAlert2 if exists in session
-        @if(session('success'))
-            Swal.fire({
-                icon: 'success',
-                title: 'Success',
-                text: "{{ session('success') }}",
-            });
-        @endif
     });
 </script>
 @endsection 
